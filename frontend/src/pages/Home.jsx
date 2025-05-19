6431d2f1a3604bab7fba8970b56a5a1264b53c17
Andres101215 <andres.puentes02@uptc.edu.co>
2025-05-19 12:45:38 -0500

    doc-aviso

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 function Home() {
     return (
       <div>
+         <h1>PARA PROBAR LA APLICACION DESPLIEGUEN LOS SERVICIOS DE AUTH SERVICE.PRODUCT SERVICE ,CART SERVICE ,PARA REGISTRAR USUARIO ES NECESARIO DESPLEGAR EL SERVICIO DEL CARRITO VAN ENLAZADOS</h1>
         <h1>Bienvenido a ConectArte</h1>
         <p>Conecta artesanos y compradores en un solo lugar.</p>
       </div>
