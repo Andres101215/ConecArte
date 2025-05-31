28d4522e3d06f918d2a94f421a8fcc262de08a39
Andres101215 <andres.puentes02@uptc.edu.co>
2025-05-31 08:56:11 -0500

    ajuste-api gateway

--- BackEnd/api-gateway/index.js
+++ BackEnd/api-gateway/index.js
@@ -3,8 +3,11 @@ const cors = require('cors');
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5050;
-app.use(cors({ origin: 'http://localhost:3000' }));
-
+app.use(cors({
+  origin: ['https://conecarte-v7d3.onrender.com','http://localhost:3000'], // tu frontend desplegado
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  credentials: true // si usas cookies o autenticación basada en sesiones
+}));
 // Importar rutas proxy
 require('./routes/user')(app);
 require('./routes/seller')(app);
