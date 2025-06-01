8d9019e981fe990a6c63d3976c5380664aa6c51b
Andres101215 <andres.puentes02@uptc.edu.co>
2025-06-01 18:17:20 -0500

    bug

--- BackEnd/api-gateway/routes/billing.js
+++ BackEnd/api-gateway/routes/billing.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 const BILLING_SERVICE_URL = process.env.BILLING_SERVICE_URL;
 
 module.exports = (app) => {
-  app.use('/facturaciones', createProxyMiddleware({
+  app.use('/facturas', createProxyMiddleware({
     target: BILLING_SERVICE_URL,
     changeOrigin: true
   }));
